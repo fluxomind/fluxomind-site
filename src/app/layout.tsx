3ce8b89d4f648efed78898e9dcbc0f6d0cbf7117
ralfo-nunes <ralfo.nunes@fluxomind.com>
2025-08-06 13:59:01 -0300

    Remove lang attribute from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt">
+    <html>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
